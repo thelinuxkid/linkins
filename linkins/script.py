f3bf91c8a9ba3a043f0ba4a11c2347e9b4a3c8be
Andres Buritica <andres@thelinuxkid.com>
2012-10-04 14:19:03 -0700

    Add linkins.script
    - Runs a script and send its output to log with a custom format

---
+++ linkins/script.py
@@ -0,0 +1,35 @@
+import logging
+import subprocess
+
+log = logging.getLogger(__name__)
+log.propagate = False
+handler = logging.StreamHandler()
+fmt = logging.Formatter(
+    fmt='%(script)s: %(stream)s: %(message)s',
+    )
+handler.setFormatter(fmt)
+log.addHandler(handler)
+
+def _logscript(fp, **kwargs):
+    for line in fp:
+        line = line.strip()
+        log.info(line, extra=kwargs)
+
+def runscript(path, name):
+    proc = subprocess.Popen(
+        [path],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    with proc.stderr as fp:
+        _logscript(
+            fp,
+            script=name,
+            stream='STDERR',
+        )
+    with proc.stdout as fp:
+        _logscript(
+            fp,
+            script=name,
+            stream='STDOUT',
+        )
