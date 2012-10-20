b4f64fb0c322c7267a66c2142c8d16327cb97e71
Andres Buritica <andres@thelinuxkid.com>
2012-10-20 03:30:06 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open(path) as fp:
 
 setup(
     name='linkins',
-    version='0.0.3',
+    version='0.0.4',
     description='linkins -- Safely link directory structures',
     long_description=long_description,
     author='Andres Buritica',
